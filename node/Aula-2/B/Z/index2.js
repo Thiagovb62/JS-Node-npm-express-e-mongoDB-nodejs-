a04bedcf804863c5333afa349df865512f724c1d
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-06-11 15:36:01 -0300

    exports e import part2

---
+++ node/Aula-2/B/Z/index2.js
@@ -0,0 +1,3 @@
+const Cachorro = require('../C/D/index.js');
+
+module.exports = Cachorro;
---
+++ node/Aula-2/app.js
@@ -0,0 +1,7 @@
+//const Cachorro = require('./B/Z/index2.js');
+//console.log( Cachorro);
+
+const Cachorro = require('./B/Z/index2.js');
+
+const c1 = new Cachorro('Rex'); 
+c1.latir();
