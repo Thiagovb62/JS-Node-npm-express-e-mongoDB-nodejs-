7e2e0f3812812b9e536dc5df21968d6f3cc21db6
Thiago Viana <thiagovb62@gmail.com>
2022-06-15 14:40:31 -0300

    express-view

---
+++ Express/Aula-5/index.js
@@ -0,0 +1,14 @@
+const { application, query } = require("express");
+const express = require("express");
+const app = express();
+const port = 3000;  
+const route = require("./routes");
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+app.use(route);
+app.set("views", path.resolve(__dirname,'src', "views"));
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
