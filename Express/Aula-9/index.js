a3f9be5d90d56b8873fba3657080864ac96178d2
Thiago Viana <thiagovb62@gmail.com>
2022-06-21 11:50:54 -0300

    realizando a primeira conexao com banco e contruindo nosso homeModel

---
+++ Express/Aula-9/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+
+const { application, query } = require("express");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() =>{
+    console.log('conectei no banco de dados');
+    app.emit('appStarted');
+})
+  .catch(e => console.log(e))
+const port = 3000;
+
+const route = require("./routes");
+const path = require("path");
+const {middlewareGlobal} = require("./src/middlewares/index");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname,"public")));
+
+app.use(middlewareGlobal)
+app.use(route);
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.on("appStarted", () => {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+});
