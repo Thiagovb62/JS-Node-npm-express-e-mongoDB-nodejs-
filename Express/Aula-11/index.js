6756e780237907e5d94bce10ead2f66ca3050bff
Thiago Viana <thiagovb62@gmail.com>
2022-06-27 15:51:52 -0300

    apredendo a implementar tokens helmet e csrfToken().

---
+++ Express/Aula-11/index.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+
+const { application, query } = require("express");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log("conectei no banco de dados");
+    app.emit("appStarted");
+  })
+  .catch((e) => console.log(e));
+const port = 3000;
+
+const helmet = require("helmet");
+const crsf = require("csurf");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const flash = require("connect-flash");
+
+const route = require("./routes");
+const path = require("path");
+const { middlewareGlobal, checkCsrfError, checkCsrfGlobal} = require("./src/middlewares/index");
+
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session ({
+  secret:'lcklslkalkadaldslçlsdlaçepowéç',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave:false,
+  saveUninitialized: false,
+  cookie:{
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly:true
+  }
+  
+})
+
+app.use(sessionOptions);
+app.use(flash());
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(crsf())
+
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(checkCsrfGlobal);
+app.use(route);
+
+
+app.on("appStarted", () => {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+});
